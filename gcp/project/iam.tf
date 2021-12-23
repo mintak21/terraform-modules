9b18966214fc392290775a385ca252ff0325c3df
mintak-Macbook <mintak21@gmail.com>
2021-12-23 18:53:44 +0900

    add workload identity pool

---
+++ gcp/project/iam.tf
@@ -0,0 +1,25 @@
+resource "google_iam_workload_identity_pool" "github" {
+  provider                  = google-beta
+  workload_identity_pool_id = "github"
+  display_name              = "github"
+  description               = "for github actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  provider                           = google-beta
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "github actions provider"
+  description                        = "OIDC identity pool provider for execute github actions"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.owner"      = "assertion.repository_owner"
+    "attribute.refs"       = "assertion.ref"
+  }
+  attribute_condition = var.attribute_condition
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
--- gcp/project/variables.tf
+++ gcp/project/variables.tf
@@ -11,5 +11,11 @@ variable "billing_account_id" {
 variable "additional_enable_services" {
   type        = set(string)
   description = "追加で有効にするAPIサービスリスト"
-  default = []
+  default     = []
+}
+
+variable "attribute_condition" {
+  type        = string
+  description = "Workload Identity Pool Providerに設定する属性条件"
+  default     = ""
 }
