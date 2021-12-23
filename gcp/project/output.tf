d4b2c8d90ec4fd888e8c694eff22596ac532c9e7
mintak-Macbook <mintak21@gmail.com>
2021-12-24 00:26:08 +0900

    add output

--- gcp/project/output.tf
+++ gcp/project/output.tf
@@ -1,4 +1,19 @@
-output "id" {
+output "project_id" {
   description = "プロジェクトID"
   value       = google_project.this.project_id
+}
+
+output "service_account_github_actions_email" {
+  description = "Actionsで使用するサービスアカウント"
+  value       = google_service_account.github_actions.email
+}
+
+output "iam_workload_identity_pool_github_id" {
+  description = "Workload Identity Pood ID"
+  value       = google_iam_workload_identity_pool.github.id
+}
+
+output "google_iam_workload_identity_pool_provider_github_id" {
+  description = "Workload Identity Pood Provider ID"
+  value       = google_iam_workload_identity_pool_provider.github.id
 }
